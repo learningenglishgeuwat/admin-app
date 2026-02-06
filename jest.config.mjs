bf840f93b3673031bf696bf7d29d2fe401751ebd
learningenglishgeuwat <learningenglishgeuwat@gmail.com>
2026-02-06 15:52:23 +0700

    fix tes2

--- jest.config.mjs
+++ jest.config.mjs
@@ -5,6 +5,8 @@ const createJestConfig = nextJest({ dir: './' })
 const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['<rootDir>/__tests__/**/*.test.(ts|tsx|js|jsx)'],
+  testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1'
   }
